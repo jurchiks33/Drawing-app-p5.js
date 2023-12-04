9e33b637fda0251a4dd7e1c6c4f547143efdcf5e
jurchiks33 <jurisslisans@gmail.com>
2023-12-04 14:29:46 +0100

    SprayCanTool constructor

---
+++ sprayCanTool.js
@@ -0,0 +1,16 @@
+
+function SprayCanTool() {
+    this.name = "sprayCanTool";
+    this.icon = "assets/sprayCan.jpg";
+    this.points = 13;
+    this.spread = 10;
+
+    this.draw = function(){
+        if(mouseIsPressed){
+            for(var i = 0; i < this.points; i++){
+                point(random(mouseX-this.spread, mouseX + this.spread),
+                    random(mouseY-this.spread, mouseY+this.spread));
+            }
+        }
+    }
+};
