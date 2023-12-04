b413b855cb0c26573d19279f10dbbdb341cd5c16
jurchiks33 <jurisslisans@gmail.com>
2023-12-04 15:39:49 +0100

    work on a shapetool

--- shapeTools.js
+++ shapeTools.js
@@ -1,4 +1,22 @@
 function ShapeTools() {
     this.name = "ShapeTools";
     this.icon = "assets/shapeToolIcon.jpg";
+
+    //Default shape
+    this.selectedShape = 'rectangle';
+
+    //starting and ending points of shape
+    this.startX = null;
+    this.startY = null;
+
+    //this function changes selected shape.
+    this.selectedShape = function(shape) {
+        this.selectedShape = shape;
+    }
+
+    //Mouse pressed and released events
+    this.mousePressed = function() {
+        this.startX = mouseX;
+        this.startY = mouseY;
+    }
 }
