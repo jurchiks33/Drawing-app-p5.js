819f8a34a46a96184738fcc7baa758dfdb3261c8
jurchiks33 <jurisslisans@gmail.com>
2023-12-05 17:35:40 +0100

    undoredomanager

--- undoRedoManager.js
+++ undoRedoManager.js
@@ -4,24 +4,33 @@ function UndoRedoManager() {
 
     this.saveState = function() {
         this.redoStack = [];
-    }
+    
 
     this.undoStack.push(getCanvasImage());
 };
 
-this.undo = function() {
-    if (this.undoStack.lenght > 0) {
-        this.redoStack.push(getCanvasImage());
-        var previousState = this.undoStack.pop();
-        loadCanvasImage(previousState);
-    }
-};
+    this.undo = function() {
+        if (this.undoStack.lenght > 0) {
+            this.redoStack.push(getCanvasImage());
+            var previousState = this.undoStack.pop();
+            loadCanvasImage(previousState);
+        }
+    };
 
-this.redo = function() {
-    if (this.redoStack.lenght > 0) {
-        this.undoStack.push(getCanvasImage());
-        var nextState = this.redoStack.pop();
-        loadCanvasImage(nextState);
-    }
-};
+    this.redo = function() {
+        if (this.redoStack.lenght > 0) {
+            this.undoStack.push(getCanvasImage());
+            var nextState = this.redoStack.pop();
+            loadCanvasImage(nextState);
+        }
+    };
 
+    function getCanvasImage() {
+        return getCanvasImage(0, 0, width, height);
+    }
+
+    function loadCanvasImage(img) {
+        clear();
+        Image(img, 0, 0);
+    }
+}
