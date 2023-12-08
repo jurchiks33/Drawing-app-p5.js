6eade509f0e953e331ddd5b446ca8d5d3116076f
jurchiks33 <jurisslisans@gmail.com>
2023-12-08 17:26:26 +0100

    brush

--- customBrush.js
+++ customBrush.js
@@ -2,5 +2,11 @@ function CustomBrush() {
     this.name = "Custom Brush";
     this.icon = "assets/brush.jpg";
 
-    
+    this.draw = function() {
+        if (mouseIsPressed) {
+            stroke(0);
+            strokeWeight(1);
+            AudioListener(pmouseX, pmouseY, mouseX, mouseY);
+        }
+    };
 }
