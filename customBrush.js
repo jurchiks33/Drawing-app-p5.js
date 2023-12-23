b033b3df2b7f307e0ad422ff0e37f244bba71238
jurchiks33 <jurisslisans@gmail.com>
2023-12-23 15:41:17 +0100

    updating global variables

--- customBrush.js
+++ customBrush.js
@@ -4,20 +4,9 @@ function CustomBrush() {
 
     this.draw = function() {
         if (mouseIsPressed) {
-            stroke(0); 
-            strokeWeight(this.brushSize); 
+            stroke(0);
+            strokeWeight(brushSize); 
             line(pmouseX, pmouseY, mouseX, mouseY);
         }
     };
-
-    this.populateOptions = function() {
-        // First, clear the previous options
-        select('.options').html('');
-        // Then create the slider
-        var brushSlider = createSlider(1, 100, this.brushSize);
-        brushSlider.parent(select('.options'));
-        brushSlider.input(() => {
-            this.brushSize = brushSlider.value();
-    });
-};
 }
