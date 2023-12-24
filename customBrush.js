5a92a371238806bb940328ae87ae4be5dae92367
jurchiks33 <jurisslisans@gmail.com>
2023-12-24 11:58:20 +0100

    commenting work

--- customBrush.js
+++ customBrush.js
@@ -3,7 +3,7 @@ function CustomBrush() {
     this.name = "Custom Brush";
     this.icon = "assets/brush.jpg";
 
-    //Draw function whic work all the time when mouse is pressed.
+    //Draw function which work all the time when mouse is pressed.
     this.draw = function() {
         if (mouseIsPressed) {
             let brushDensity = 10; // Number of points in the brush
@@ -15,13 +15,14 @@ function CustomBrush() {
                 let offsetY = random(-brushSpread, brushSpread); //offsetY for random stroke displacement.
                 let alphaValue = random(50, 255); // Random opacity for each stroke
 
+                //use of stroke color from colour pallette and applying random alpha value for transparency.
                 stroke(colourP.selectedColour);
                 strokeWeight(random(1, brushSize / 2)); // Varying stroke weight
                 stroke(color(red(colourP.selectedColour), 
                         green(colourP.selectedColour), 
                         blue(colourP.selectedColour), alphaValue));
                 
-                line(pmouseX + offsetX, pmouseY + offsetY, mouseX + offsetX, mouseY + offsetY);
+                line(pmouseX + offsetX, pmouseY + offsetY, mouseX + offsetX, mouseY + offsetY); //Draw line from previous mouse posittion and offset it by random value
             }
         }
     };
