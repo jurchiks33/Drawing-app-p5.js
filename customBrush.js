444267d3792acfc5c8d8cfc17cb3f41ebf9d50fb
jurchiks33 <jurisslisans@gmail.com>
2023-12-24 11:37:45 +0100

    updated brush effects

--- customBrush.js
+++ customBrush.js
@@ -4,9 +4,22 @@ function CustomBrush() {
 
     this.draw = function() {
         if (mouseIsPressed) {
-            stroke(colourP.selectedColour);
-            strokeWeight(brushSize); 
-            line(pmouseX, pmouseY, mouseX, mouseY);
+            let brushDensity = 10; // Number of points in the brush
+            let brushSpread = brushSize / 2; // Spread of the brush strokes
+
+            for (let i = 0; i < brushDensity; i++) {
+                let offsetX = random(-brushSpread, brushSpread);
+                let offsetY = random(-brushSpread, brushSpread);
+                let alphaValue = random(50, 255); // Random opacity for each stroke
+
+                stroke(colourP.selectedColour);
+                strokeWeight(random(1, brushSize / 2)); // Varying stroke weight
+                stroke(color(red(colourP.selectedColour), 
+                        green(colourP.selectedColour), 
+                        blue(colourP.selectedColour), alphaValue));
+                
+                line(pmouseX + offsetX, pmouseY + offsetY, mouseX + offsetX, mouseY + offsetY);
+            }
         }
     };
 }
