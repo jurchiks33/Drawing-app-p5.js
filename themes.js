5071ff2bad5bf611a4e5d36cff54b07830e82e8b
jurchiks33 <jurisslisans@gmail.com>
2024-03-02 21:08:43 +0100

    testing

--- themes.js
+++ themes.js
@@ -1,32 +1,32 @@
 //in this file we have themes for app. linked to main sketch.js file.
 
 //below are themes for app, starting with 4 and after adding more as needed.
-// var themes = {
-//     "Christmas": {
-//         backgroundColor: "#FF0000",     // Red
-//         buttonColor: "#FFFFFF",         // White
-//         slideColor: "#00FF00",          // Green
-//         toolIconColor: "#FFFFFF",       // White
-//     },
-//     "City": {
-//         backgroundColor: "#707070",     // Grey
-//         buttonColor: "#000000",         // Black
-//         slideColor: "#FFD700",          // Gold
-//         toolIconColor: "#FFFFFF",       // White
-//     },
-//     "Classic": {
-//         backgroundColor: "#FFFFFF",     // White
-//         buttonColor: "#000000",         // Black
-//         slideColor: "#C0C0C0",          // Silver
-//         toolIconColor: "#000000",       // Black
-//     },
-//     "Cosmic": {
-//         backgroundColor: "#0B3D91",     // Dark Blue
-//         buttonColor: "#FFA500",         // Orange
-//         slideColor: "#800080",          // Purple
-//         toolIconColor: "#FFFFFF",       // White
-//     }   
-// };
+var themes = {
+    "Christmas": {
+        backgroundColor: "#FF0000",     // Red
+        buttonColor: "#FFFFFF",         // White
+        slideColor: "#00FF00",          // Green
+        toolIconColor: "#FFFFFF",       // White
+    },
+    "City": {
+        backgroundColor: "#707070",     // Grey
+        buttonColor: "#000000",         // Black
+        slideColor: "#FFD700",          // Gold
+        toolIconColor: "#FFFFFF",       // White
+    },
+    "Classic": {
+        backgroundColor: "#FFFFFF",     // White
+        buttonColor: "#000000",         // Black
+        slideColor: "#C0C0C0",          // Silver
+        toolIconColor: "#000000",       // Black
+    },
+    "Cosmic": {
+        backgroundColor: "#0B3D91",     // Dark Blue
+        buttonColor: "#FFA500",         // Orange
+        slideColor: "#800080",          // Purple
+        toolIconColor: "#FFFFFF",       // White
+    }   
+};
 
 // Function to apply a theme.
 function applyTheme(themeName) {
