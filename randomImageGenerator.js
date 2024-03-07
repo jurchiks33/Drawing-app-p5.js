f7a520551ace9ff2d435df440d0c6ea49e2b6669
jurchiks33 <jurisslisans@gmail.com>
2024-03-07 17:28:16 +0100

    debugging random image generator

--- randomImageGenerator.js
+++ randomImageGenerator.js
@@ -3,11 +3,11 @@
 function addRandomImageGenerator() {
     let pixelInput, drawButton;
     pixelInput = createInput('1000');
-    pixelInput.position(10, height + 40);  // adjust position if needed.
+    pixelInput.position(10, height + 60);  // adjust position if needed.
     pixelInput.size(100);
 
     drawButton = createButton('Draw Random');
-    drawButton.position(120, height + 40); //Adjust position if needed.
+    drawButton.position(120, height + 60); //Adjust position if needed.
     drawButton.mousePressed(() => {
         const totalPixels = parseInt(pixelInput.value());
         drawRandomPixels(totalPixels);
@@ -15,11 +15,12 @@ function addRandomImageGenerator() {
 }
 
 function drawRandomPixels(totalPixels) {
+    loadPixels();   //preparing canvas for pixel manipulation.
     for (let i = 0; i < totalPixels; i++) {
         let x = floor(random(width));
         let y = floor(random(height));
         let col = color(random(255), random(255), random(255));
-        setInterval(x, y, col);
+        set(x, y, col);
     }
     updatePixels();
 }
