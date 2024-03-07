a6ff7a56d215427d07186c217888f667f913a6ef
jurchiks33 <jurisslisans@gmail.com>
2024-03-07 16:58:11 +0100

    random image

--- randomImageGenerator.js
+++ randomImageGenerator.js
@@ -2,4 +2,9 @@
 
 function addRandomImageGenerator() {
     let pixelInput, drawButton;
+    pixelInput = createInput('1000');
+    pixelInput.position(10, height + 40);  // adjust position if needed.
+    pixelInput.size(100);
+
+    drawButton = createButton('Draw Random');
 }
