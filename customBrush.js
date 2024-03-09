70298cd03dfe20dfb74190a23c68d02e8ec5d20a
jurchiks33 <jurisslisans@gmail.com>
2024-03-09 17:11:30 +0100

    refactoring

--- customBrush.js
+++ customBrush.js
@@ -6,23 +6,23 @@ function CustomBrush() {
     //Draw function which work all the time when mouse is pressed.
     this.draw = function() {
         if (mouseIsPressed) {
-            let brushDensity = 10; // Number of points in the brush
-            let brushSpread = brushSize / 2; // Spread of the brush strokes
+            let brushDensity = 10;              // Number of points in the brush.
+            let brushSpread = brushSize / 2;    // Spread of the brush strokes.
 
-            //loop for multiple brush strokes
+            //loop for multiple brush strokes.
             for (let i = 0; i < brushDensity; i++) {
-                let offsetX = random(-brushSpread, brushSpread); //offsetX for random stroke displacement
-                let offsetY = random(-brushSpread, brushSpread); //offsetY for random stroke displacement.
-                let alphaValue = random(50, 255); // Random opacity for each stroke
+                let offsetX = random(-brushSpread, brushSpread);    //offsetX for random stroke displacement.
+                let offsetY = random(-brushSpread, brushSpread);    //offsetY for random stroke displacement.
+                let alphaValue = random(50, 255);                   // Random opacity for each stroke.
 
                 //use of stroke color from colour pallette and applying random alpha value for transparency.
                 stroke(colourP.selectedColour);
-                strokeWeight(random(1, brushSize / 2)); // Varying stroke weight
+                strokeWeight(random(1, brushSize / 2));             // Varying stroke weight.
                 stroke(color(red(colourP.selectedColour), 
                         green(colourP.selectedColour), 
                         blue(colourP.selectedColour), alphaValue));
                 
-                line(pmouseX + offsetX, pmouseY + offsetY, mouseX + offsetX, mouseY + offsetY); //Draw line from previous mouse posittion and offset it by random value
+                line(pmouseX + offsetX, pmouseY + offsetY, mouseX + offsetX, mouseY + offsetY); //Draw line from previous mouse posittion and offset it by random value.
             }
         }
     };
